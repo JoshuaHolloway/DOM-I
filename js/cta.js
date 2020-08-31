d092b6c749276e64a3331e9b66a425f13ada9624
JoshuaHolloway <jhollow6@asu.edu>
2020-08-31 18:36:48 -0500

    .cta > img

--- js/cta.js
+++ js/cta.js
@@ -11,8 +11,12 @@ print(h1_data);
 // .cta > .cta-text > h1
 const h1_str = `${h1_data[0]} <br> ${h1_data[1]}<br> ${h1_data[2]}`;
 h1.textContent = h1_str;
-print(cta);
 
 // .cta > .cta-text > h1
 const button = document.querySelector('.cta > .cta-text > button');
 button.textContent = cta_data.button;
+
+// .cta > img
+const img = document.querySelector('.cta > img');
+img.setAttribute('src', cta_data['img-src']);
+print(cta);
