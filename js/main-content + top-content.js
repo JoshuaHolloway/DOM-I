3c5536ee9797e81808b7a50fc563370a36e0cba6
JoshuaHolloway <jhollow6@asu.edu>
2020-08-31 18:45:35 -0500

    .main-content > .top-content > .text-content > h4

---
+++ js/main-content + top-content.js
@@ -0,0 +1,4 @@
+const text_content = document.querySelector('.main-content > .top-content > .text-content > h4');
+
+text_content.textContent = siteContent['main-content']['features-h4'];
+print(text_content);
