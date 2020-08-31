87afb2124b63e8a0cd374fd360cba9625b1912bd
JoshuaHolloway <jhollow6@asu.edu>
2020-08-31 18:18:37 -0500

    header > img

--- js/main.js
+++ js/main.js
@@ -1,16 +1,41 @@
-console.log('Hello from JOSH');
+console.log('Hello from JOSH-2');
+
+const nav_elems = siteContent.nav;
+const nav_keys = keys(nav_elems);
+// print(nav_keys);
+
+// print('nav vals:');
+const nav_vals = vals(nav_elems);
+// print(nav_vals)
+
+// filter over nav-items for string matching 'nav-item-'
+const filtered = [];
+const filtered2 = [];
+nav_keys.forEach((elem, idx) => {
+    // https://regexr.com/
+    const reg_ex = /nav-item-./g;
+    const match = elem.match(reg_ex);
+    if (match !== null)
+        filtered.push(nav_vals[idx]); // nav-item-.
+    else
+        filtered2.push(nav_vals[idx]); // all else
+});
+// print(filtered);
+// print(filtered2);
 
 const nav = document.querySelector('nav');
-console.log(nav);
+filtered.forEach((elem, idx) => {
 
-const as = Array.from(nav.children);
-console.log(as);
-
-const a_tag = (a, href, text) => {
-    a.textContent = text;
-    a.setAttribute('href', href);
-};
-
-as.forEach((a) => {
-    a_tag(a, 'josh-href', 'josh-text');
+    const new_elem = document.createElement('a');
+    new_elem.textContent = elem;
+    new_elem.setAttribute('href', '#');
+    
+    nav.appendChild(new_elem);
 });
+print(nav);
+
+
+
+const img = document.querySelector('header > img');
+img.setAttribute('src', filtered2[0]);
+print(img);
