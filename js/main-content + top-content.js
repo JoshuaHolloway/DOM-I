0fe067b6c11b3a6ac474392fe64a853baded7a2c
JoshuaHolloway <jhollow6@asu.edu>
2020-08-31 18:51:47 -0500

    .main-content > .top-conent > .text-content:nth-child(2) > h4

--- js/main-content + top-content.js
+++ js/main-content + top-content.js
@@ -1,10 +1,16 @@
-const text_content_h4 = document.querySelector('.main-content > .top-content > .text-content > h4');
+const text_content1_h4 = document.querySelector('.main-content > .top-content > .text-content:nth-child(1) > h4');
 
 // .main-content > .top-content > .text-content > h4
-text_content_h4.textContent = siteContent['main-content']['features-h4'];
-print(text_content_h4);
+text_content1_h4.textContent = siteContent['main-content']['features-h4'];
+print(text_content1_h4);
 
 // .main-content > .top-content > .text-content > p
-const text_content_p = document.querySelector('.main-content > .top-content > .text-content > p');
-text_content_p.textContent = siteContent['main-content']['features-content'];
-print(text_content_p);
+const text_content1_p = document.querySelector('.main-content > .top-content > .text-content:nth-child(1) > p');
+text_content1_p.textContent = siteContent['main-content']['features-content'];
+print(text_content1_p);
+
+// =======================================
+const text_content2_h4 = document.querySelector('.main-content > .top-content > .text-content:nth-child(2) > h4');
+
+text_content2_h4.textContent = siteContent['main-content']['about-h4'];
+print(text_content2_h4);
