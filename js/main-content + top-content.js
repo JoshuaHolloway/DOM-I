b7c84ac6ac268447ffd7763a305d2960c5208377
JoshuaHolloway <jhollow6@asu.edu>
2020-08-31 18:48:10 -0500

    .main-content > .top-content > .text-content > p

--- js/main-content + top-content.js
+++ js/main-content + top-content.js
@@ -1,4 +1,10 @@
-const text_content = document.querySelector('.main-content > .top-content > .text-content > h4');
+const text_content_h4 = document.querySelector('.main-content > .top-content > .text-content > h4');
 
-text_content.textContent = siteContent['main-content']['features-h4'];
-print(text_content);
+// .main-content > .top-content > .text-content > h4
+text_content_h4.textContent = siteContent['main-content']['features-h4'];
+print(text_content_h4);
+
+// .main-content > .top-content > .text-content > p
+const text_content_p = document.querySelector('.main-content > .top-content > .text-content > p');
+text_content_p.textContent = siteContent['main-content']['features-content'];
+print(text_content_p);
