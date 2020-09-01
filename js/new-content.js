25a5ac43d07f255b95c244770b7c167de3075805
JoshuaHolloway <jhollow6@asu.edu>
2020-09-01 09:24:29 -0500

    stretch

--- js/new-content.js
+++ js/new-content.js
@@ -1,6 +1,6 @@
 // * [ ] Change the color of the navigation text to be green.
 const as = qsAll('nav > a');
-as.forEach((elem) => elem.style.color = 'green');
+as.forEach((elem) => color(elem, 'green'));
 
 
 // * [ ] Utilize `.appendChild()` and `.prepend()` to add two new items to the navigation system. You can call them whatever you want.
@@ -15,7 +15,3 @@ const new_elem2 = document.createElement('a');
 new_elem2.textContent = 'josh-2';
 const nav1 = qs('nav');
 nav1.append(new_elem2);
-
-
-
-// * [ ] Check your work by looking at the [original html](original.html) in the browser
--- js/~.js
+++ js/~.js
@@ -5,3 +5,5 @@ const vals = (obj) => Object.values(obj);
 
 const qs = (str) => document.querySelector(str);
 const qsAll = (str) => Array.from(document.querySelectorAll(str));
+
+const color = (elem, color) => elem.style.color = color;
