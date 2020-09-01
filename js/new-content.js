c57d1407a17449a91f8203ceb670963493cb3907
JoshuaHolloway <jhollow6@asu.edu>
2020-08-31 20:22:36 -0500

    defer script loading

---
+++ js/new-content.js
@@ -0,0 +1,3 @@
+// * [ ] Change the color of the navigation text to be green.
+// * [ ] Utilize `.appendChild()` and `.prepend()` to add two new items to the navigation system. You can call them whatever you want.
+// * [ ] Check your work by looking at the [original html](original.html) in the browser
