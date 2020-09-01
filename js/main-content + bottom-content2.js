71ed56a1749c0fb70e6360705ec22dbd9d251382
JoshuaHolloway <jhollow6@asu.edu>
2020-08-31 19:29:32 -0500

    .main-content > .bottom-content

--- js/main-content + bottom-content2.js
+++ js/main-content + bottom-content2.js
@@ -26,17 +26,17 @@ text_contents.forEach((elem, idx) => {
         }
     }
 
-    const text_content1_h4 = document.querySelector(`.main-content > .bottom-content > .text-content:nth-child(${idx+1}) > h4`);
+    const text_content_h4 = document.querySelector(`.main-content > .bottom-content > .text-content:nth-child(${idx+1}) > h4`);
 
-    // .main-content > .bottom-content > .text-content:nth-child(1) > h4
-    text_content1_h4.textContent = siteContent['main-content'][`${str}-h4`];
-    print(text_content1_h4);
+    // .main-content > .bottom-content > .text-content:nth-child(idx+1) > h4
+    text_content_h4.textContent = siteContent['main-content'][`${str}-h4`];
+    // print(text_content_h4);
 
-    // .main-content > .bottom-content > .text-content:nth-child(1) > p
-    const text_content1_p = document.querySelector(`.main-content > .bottom-content > .text-content:nth-child(${idx+1}) > p`);
+    // .main-content > .bottom-content > .text-content:nth-child(idx+1) > p
+    const text_content_p = document.querySelector(`.main-content > .bottom-content > .text-content:nth-child(${idx+1}) > p`);
 
-    text_content1_p.textContent = siteContent['main-content'][`${str}-content`];
-    print(text_content1_p);
+    text_content_p.textContent = siteContent['main-content'][`${str}-content`];
+    // print(text_content1_p);
 
 });
 
