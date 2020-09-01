a0dd50b76c00c426965343bce2107c1edbb3997a
JoshuaHolloway <jhollow6@asu.edu>
2020-09-01 10:53:10 -0500

    completed stretch assignment

--- stretch-assignment/static/js/index.js
+++ stretch-assignment/static/js/index.js
@@ -1,37 +1,42 @@
+// Note: I have this set up for seconds, minutes, and hours but am counting in ms.
+//       Just change the variable names to match the assignment requirements.
+
 class Timer {
 
     constructor() {
         this.count = 0;
         this.seconds = 0;
         this.minutes = 0;
+        this.hours = 0;
     }
     add_count() {
         ++this.count;
         this.seconds = this.count % 60;
-        this.minutes = Math.floor(this.count / 60);
+        this.minutes = Math.floor(this.count / 60) % 60;
+        this.hours   = Math.floor(this.count / 3600) % 60;
     }
 
-    get_seconds() { return this.seconds; }
-    get_minutes() { return this.minutes; }
-}
-
-const timer = new Timer();
-
-const delay = .1e3;
-setInterval(() => {
-    const body = qs('body');
-
-    const seconds = qs('#seconds');
-    const minutes = qs('#minutes');
-
-    
-    const pad = (num, zeros) => {
+    pad = (num, zeros=2) => {
         // String(<number>).padStart(<places>, '0')
         return String(num.toString()).padStart(zeros, '0')
     };
 
-    seconds.textContent = pad(timer.get_seconds(), 2);
-    minutes.textContent = pad(timer.get_minutes(), 2);
+    get_seconds() { return this.pad(this.seconds); }
+    get_minutes() { return this.pad(this.minutes); }
+    get_hours()   { return this.pad(this.hours); }
+}
+
+const timer = new Timer();
+
+const delay = 1e-3; // 1ms.
+setInterval(() => {
+    const seconds = qs('#seconds');
+    const minutes = qs('#minutes');
+    const hours = qs('#hours');
+
+    seconds.textContent = timer.get_seconds();
+    minutes.textContent = timer.get_minutes();
+    hours.textContent =   timer.get_hours();
 
     timer.add_count();
 
