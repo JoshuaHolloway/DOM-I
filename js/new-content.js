222580de1ff71287b9d20ac6b0c675582e01f39f
JoshuaHolloway <jhollow6@asu.edu>
2020-08-31 20:33:45 -0500

    Utilize `.appendChild()` and `.prepend()` to add two new items to the navigation system.

--- js/new-content.js
+++ js/new-content.js
@@ -1,11 +1,20 @@
 // * [ ] Change the color of the navigation text to be green.
-// * [ ] Utilize `.appendChild()` and `.prepend()` to add two new items to the navigation system. You can call them whatever you want.
-// * [ ] Check your work by looking at the [original html](original.html) in the browser
-
-const nav = qs('nav');
 const as = qsAll('nav > a');
+as.forEach((elem) => elem.style.color = 'green');
 
-nav.style.color = 'green';
-as.forEach((elem) => 
-    elem.style.color = 'green'
-);
+
+// * [ ] Utilize `.appendChild()` and `.prepend()` to add two new items to the navigation system. You can call them whatever you want.
+
+const new_elem = document.createElement('h2');
+new_elem.textContent = 'Hello from new-content.js!';
+const container = qs('.container');
+container.prepend(new_elem);
+
+const new_elem2 = document.createElement('h1');
+new_elem2.textContent = 'Hello from new-content.js!';
+const nav = qs('.nav');
+container.appendChild(new_elem2);
+
+
+
+// * [ ] Check your work by looking at the [original html](original.html) in the browser
