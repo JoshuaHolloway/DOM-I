1ffbf2849d36dbae64473c3668236c29a63b2623
JoshuaHolloway <jhollow6@asu.edu>
2021-02-07 00:45:51 -0600

    contact.js

--- non-stretch-assignment/js/contact.js
+++ non-stretch-assignment/js/contact.js
@@ -1,8 +1,8 @@
-print('contact.js');
+_.print('contact.js');
 
 const h4 = document.querySelector('.contact > h4');
 h4.textContent = siteContent.contact['contact-h4'];
-print(h4);
+_.print(h4);
 
 const ps = document.querySelectorAll('.contact > p');
 
